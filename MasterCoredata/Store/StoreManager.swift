8863a8e5e32fa3f7bd62858419561eb029c978aa
Vinay Hosamane K N <vinay.k.n.hosamane@sap.com>
2021-05-18 17:48:02 +0530

    added code to share managed object between different contexts using object id.

--- MasterCoredata/Store/StoreManager.swift
+++ MasterCoredata/Store/StoreManager.swift
@@ -19,11 +19,13 @@ class StoreManager {
     func execute() {
         printSQLStoreFileDirectoryPath()
 
-        insertDataInChildContext()
+        //insertDataInChildContext()
         
-        insertRecordWithEntityDescription()
+        //insertRecordWithEntityDescription()
         
-        contextInGlobalThread()
+        //contextInGlobalThread()
+        
+        sharingManagedObjectByObjectId()
     }
     
     func insertDataInsideContextsQueue() {
@@ -212,6 +214,66 @@ class StoreManager {
             }
             self.synchronize(internalContext)
         }
+    }
+    
+    func sharingManagedObjectByObjectId() {
+        let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+        fetchRequest.predicate = NSPredicate(format: "firstName == %@", "Reshma")
+        
+        // always make sure this managed object is already saved in the store as a record, else it won't update the values when the object is not found.
+        // good thing is if the object is not loaded to persistent container, it fetches and keeps in in-memory.
+        var personId: NSManagedObjectID? = nil
+        
+        do {
+            let persons = try currentContext.fetch(fetchRequest)
+            personId = persons.first?.objectID
+            print(persons)
+        } catch(let error) {
+            print(error)
+        }
+
+        print("---- Inside main context ---\(String(describing: personId))") // unique object id of person
+        
+        // let's create a background thread.
+        DispatchQueue.global().async {
+            guard let personId = personId else {
+                return
+            }
+            // let's create a new background context
+            let tempContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType) // private queue, should use one of perform or performAndWait to execute block
+            tempContext.persistentStoreCoordinator = self.currentContext.persistentStoreCoordinator // assign persistent co-ordinator to temp context, so that when we save changes to this context, it will also save it to main context.
+            print("--Inside Temp Context--\(String(describing: personId))")
+            
+            tempContext.performAndWait {
+                //let person = tempContext.object(with: personId) as? Person
+                // let's get the copy of person object from main context
+                do {
+                    // this api to check the existence might be slow, use object(personId)
+                    guard let person = try tempContext.existingObject(with: personId) as? Person else {
+                        return
+                    }
+                    person.firstName = "Sandy"
+                    person.lastName = "Man"
+                    
+                    let car = Car(context: tempContext)
+                    car.maker = "Renault"
+                    car.model = "Kiger"
+                    car.addOwner(person)
+                    
+                    person.addToCars(car)
+                    
+                    // save changes to temp context
+                    
+                    // on save it updates the previous values of person object in store.
+                    self.synchronize(tempContext)
+                } catch let error {
+                    print(error)
+                    return
+                }
+            }
+        }
+        
+        synchronize(currentContext)
     }
     
     func synchronize(_ context: NSManagedObjectContext) {
